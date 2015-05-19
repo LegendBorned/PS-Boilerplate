488124b6e1ce91b6a0620f136c1ba94ab4760195
Example1203 <jake.christmas@gmail.com>
2015-05-19 16:33:17 -0700

    Update core.js

--- core.js
+++ core.js
@@ -343,7 +343,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['creaturephil'];
+    var systemOperators = ['exampleyt'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
